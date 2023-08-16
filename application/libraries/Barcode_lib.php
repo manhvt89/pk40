68dcb70ebfcffc18c4febb2e24d38898141fbcd0
ManhVT <manhvt89@gmail.com>
2023-08-16 16:51:02 +0700

    ok

--- application/libraries/Barcode_lib.php
+++ application/libraries/Barcode_lib.php
@@ -383,16 +383,16 @@ class Barcode_lib
 		$display_table .= "<div align='center'>" . $this->manage_display_layout($barcode_config['barcode_first_row'], $item, $barcode_config) . "</div>";
 		$barcode = $this->generate_barcode($item, $barcode_config);
 		$display_table .= "<div align='center'><img src='data:image/png;base64,$barcode' /></div></tr>";
-		$display_table .= "<div align='center'><b style='font-size:10px;'>" . $this->manage_display_layout($barcode_config['barcode_second_row'], $item, $barcode_config) . "</b> </div>";
-		$display_table .= "<div align='center'><b style='font-size:10px;'>" . $this->manage_display_layout($barcode_config['barcode_third_row'], $item, $barcode_config) . "</b>- <b class='category-barcode'>".$item['item_category']."</b></div>";
+		//$display_table .= "<div align='center'><b style='font-size:10px;'>" . $this->manage_display_layout($barcode_config['barcode_second_row'], $item, $barcode_config) . "</b> </div>";
+		//$display_table .= "<div align='center'><b style='font-size:10px;'>" . $this->manage_display_layout($barcode_config['barcode_third_row'], $item, $barcode_config) . "</b>- <b class='category-barcode'>".$item['item_category']."</b></div>";
 		$display_table .= "</div>";
 
 		$display_table .= "<div class='print-barcode_2'>";
 		$display_table .= "<div align='center'>" . $this->manage_display_layout($barcode_config['barcode_first_row'], $item, $barcode_config) . "</div>";
 		$barcode = $this->generate_barcode($item, $barcode_config);
 		$display_table .= "<div align='center'><img src='data:image/png;base64,$barcode' /></div></tr>";
-		$display_table .= "<div align='center'><b style='font-size:10px;'>" . $this->manage_display_layout($barcode_config['barcode_second_row'], $item, $barcode_config) . "</b> </div>";
-		$display_table .= "<div align='center'><b style='font-size:10px;'>" . $this->manage_display_layout($barcode_config['barcode_third_row'], $item, $barcode_config) . "</b>- <b class='category-barcode'>".$item['item_category']."</b></div>";
+		//$display_table .= "<div align='center'><b style='font-size:10px;'>" . $this->manage_display_layout($barcode_config['barcode_second_row'], $item, $barcode_config) . "</b> </div>";
+		//$display_table .= "<div align='center'><b style='font-size:10px;'>" . $this->manage_display_layout($barcode_config['barcode_third_row'], $item, $barcode_config) . "</b>- <b class='category-barcode'>".$item['item_category']."</b></div>";
 		$display_table .= "</div>";
 		
 		return $display_table;
