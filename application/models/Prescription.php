ff0f6daf8e1f978f7b123db38573214caf281759
ManhVT <manhvt89@gmail.com>
2023-10-06 20:55:36 +0700

    Đơn thuốc lấy từ mục thuốc

--- application/models/Prescription.php
+++ application/models/Prescription.php
@@ -7,12 +7,28 @@ class Prescription extends CI_Model
 	*/
 	public function get_all($rows = 0, $limit_from = 0)
 	{
-		$tenthuoc = $this->config->item('tenthuoc');
+		$tenthuoc = [];//$this->config->item('tenthuoc');
+		$this->db->select('*');
+		$this->db->from('items');
+		$this->db->where('category','Thuốc');
+		$_aTenThuocs = $this->db->get()->result_array();
+		//var_dump($_aTenThuocs);
+		//["id"]=> string(4) "6290" ["name"]=> string(21) "Bột Cefalexin 250mg" ["price"]=> string(4) "1500" ["dvt"]=> string(4) "Gói" ["hdsd"]=> string(18) "2 gói mỗi ngày" ["price_display"]=> string(7) "1.500đ" }
 		$_aItem = [];
 		$_aRS = [];
-		if(empty($tenthuoc))
+		if(empty($_aTenThuocs))
 		{
 			return [];
+		}
+		foreach($_aTenThuocs as $ten)
+		{
+			$_aTenthuoc['id'] = $ten['item_id'];
+			$_aTenthuoc['name'] = $ten['name'];
+			$_aTenthuoc['price'] = $ten['unit_price'];
+			$_aTenthuoc['dvt'] = $ten['custom1'];
+			$_aTenthuoc['hdsd'] = $ten['description'];
+			$_aTenthuoc['price_display']=number_format($ten['unit_price'],0,',','.').'đ';
+			$tenthuoc[] = $_aTenthuoc;
 		}
 		foreach($tenthuoc as $ten)
 		{
