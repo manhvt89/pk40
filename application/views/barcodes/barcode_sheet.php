7bee3faf7ad6046e7274e276cd5516deed58da35
ManhVT <manhvt89@gmail.com>
2023-10-06 17:32:11 +0700

    ok

--- application/views/barcodes/barcode_sheet.php
+++ application/views/barcodes/barcode_sheet.php
@@ -6,9 +6,87 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title><?php echo $this->lang->line('items_generate_barcodes'); ?></title>
 	<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>dist/barcode_font.css" />
-	<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>dist/barcode_print.css" />
+	
 </head>
 <style>
+	.barcode-print-area {
+	background-color: transparent;
+	outline: 1px dashed;
+	width: 188.9px;
+	height: 151.2px;
+	margin: 0px 3px 0px 3px;
+	/*transform: rotate(45deg);*/
+}
+.barcode-item-unit_price{
+	font-weight: bold;
+	line-height: 14px;
+}
+
+.print-barcode_2 {
+	width: 50mm;
+	transform: rotate(180deg);
+	padding-bottom: 5px;
+	border-spacing: 1px;
+}
+
+.print-barcode_1 {
+	width: 50mm;
+	/*outline: 1px dashed;*/
+	padding-top: 5px;
+	border-spacing: 1px;
+	height: 20mm;
+}
+
+.print-page-barcode {
+	width: 105mm;
+	/*
+outline: 1px dashed;
+border-spacing: 1px;
+*/
+	/*width: 420px;*/
+	margin: auto;
+}
+
+.category-barcode {
+	transform: rotate(90deg);
+}
+
+.buttonpr {
+	width: 105mm;
+	margin: auto;
+	padding: 25px;
+	text-align: center;
+}
+
+.bt-print-barcode {
+	width: 40mm;
+	margin: auto;
+	height: 12mm;
+	font-size: 25px;
+	background-color: gray;
+	font-family: 'Times New Roman', Times, serif;
+}
+
+.store_name {
+	font-size: 14px;
+	font-family: initial;
+}
+
+@media print {
+	.pagebreak {
+		clear: both;
+		page-break-after: always;
+	}
+	.buttonpr {
+		display: none;
+	}
+	body {
+		margin: 0;
+	}
+	#register_wrapper {
+		display: none;
+	}
+}
 </style>
 
 <body class=<?php echo "font_".$this->barcode_lib->get_font_name($barcode_config['barcode_font']); ?> 
