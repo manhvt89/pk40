265e6388e95b6d5c674de132715c9847743d9518
ManhVT <manhvt89@gmail.com>
2024-08-21 14:27:27 +0700

    o

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -138,7 +138,7 @@
 						<!-- <a class="navbar-brand hidden-sm" href="<?php echo site_url(); ?>">ESS</a> -->
 						<!-- Những mục bạn muốn hiển thị ra ngoài menu collapse -->
 						<ul class="nav navbar-nav visible-xs-inline-block">
-						<ul class="nav navbar-nav navbar-right">
+						
 							<?php foreach($allowed_modules as $module): ?>
 							<li class="<?php echo $module->module_key == $this->uri->segment(1)? 'active': ''; ?>">
 								<a href="<?php echo site_url("$module->module_key");?>" title="<?php echo $this->lang->line("module_".$module->module_key);?>" class="menu-icon">
@@ -147,7 +147,7 @@
 								</a>
 							</li>
 							<?php endforeach; ?>
-						</ul>
+						
 						</ul>
 					</div>
 
