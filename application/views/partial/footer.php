4b8cb9a35e11a82250d0b3bb3550260ab1105e46
ManhVT <manhvt89@gmail.com>
2024-08-25 00:01:33 +0700

    ok

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -5,7 +5,7 @@
 		<div class="jumbotron push-spaces">
 			<strong>Bạn đang sử dụng phần mềm quản lý phòng khám mắt PK40
   			version V2.0.1</strong>.
-			Liên hệ <a href="https://zalo.me/0936111617" target="_blank">0936111917</a>
+			Liên hệ Ông Vũ Thành Mạnh <a href="https://zalo.me/0936111617" target="_blank">0936111917</a>
 			</br>
 			Chuyên gia tư vấn An Ninh Thông Tin theo tiêu chuẩn ISO27001, 5S, Quản lý chất lượng, Chuyển đổi số;
 		</div>
