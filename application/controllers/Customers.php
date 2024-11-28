3236f8ad796441d8bd7f04e2bee8734498b2e389
ManhVT <manhvt89@gmail.com>
2024-11-28 08:36:50 +0700

    o

--- application/controllers/Customers.php
+++ application/controllers/Customers.php
@@ -95,12 +95,14 @@ class Customers extends Persons
         } */
 		if($info->age == '')
 		{
+			/*
 			if($this->config->item('dob_type') == 'only_year')
 			{
 				$info->age = '1970';
 			} else {
 				$info->age = '01/01/1970';
 			}
+			*/
 		}
 		//var_dump($info);
 		$info->first_name = get_fullname($info->first_name, $info->last_name);
